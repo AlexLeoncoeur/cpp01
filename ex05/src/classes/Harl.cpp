8205c575484767193b70b5f98bb0074050842f1f
root <root@DESKTOP-37LOKJS.>
2025-03-11 17:39:08 +0100

    ex05: finished

--- ex05/src/classes/Harl.cpp
+++ ex05/src/classes/Harl.cpp
@@ -1,5 +1,30 @@
 #include "../../include/Harl.hpp"
 
+void	Harl::debug()
+{
+	std::cout 	<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+				<< std::endl;
+}
+
+void	Harl::info()
+{
+	std::cout	<< "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+				<< std::endl;
+
+}
+
+void	Harl::warning()
+{
+	std::cout 	<< "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
+				<< std::endl;
+}
+
+void	Harl::error()
+{
+	std::cout	<< "This is unacceptable! I want to speak to the manager now."
+				<< std::endl;
+}
+
 Harl::Harl()
 {
 }
@@ -14,6 +39,12 @@ void	Harl::complain(std::string level)
 	void		(Harl::*pointerToMember[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
 	for (int i = 0; i < 4; i++)
-		if (level.compare(errors[i]))
+	{
+		if (!level.compare(errors[i]))
+		{
 			(this->*pointerToMember[i])();
+			return ;
+		}
+	}
+	std::cerr << "error: not valid level" << std::endl, exit(1);
 }
